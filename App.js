118d995a30a2f9cd88ed1775643132cfd610554e
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-04 18:20:46 -0300

    apostila05(app-lembretes): mais ou menos na secao 2.11
    OBS: Fernando deverá fazer o exercício

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button, 
+  StyleSheet, 
+  TextInput, 
+  View 
+} from 'react-native';
+//hook
+import { useState } from 'react'
 
 export default function App() {
+  const [lembrete, setLembrete] = useState('')
+  const capturarLembrete = (lembreteDigitado) => {
+    console.log(lembreteDigitado)
+  }
+
+  const adicionarLembrete = () => {
+    console.log(lembrete)
+  } 
+  // single source of truth SSOT
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput 
+          placeholder='Lembrar...'
+          style={styles.lembreteTextInput}
+          // onChangeText={capturarLembrete}
+          //lição de casa para o Fernando...
+          // onChange={(e) => capturarLembrete(e.target.value)}
+          // value={lembrete}
+        />          
+        <Button 
+          title='Adicionar'
+          onPress={adicionarLembrete}
+        />
+      </View>
+      <View>
+
+      </View>      
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
   },
+  lembreteTextInput: {
+    borderBottomColor: '#CCC', 
+    borderBottomWidth: 1, 
+    marginBottom: 4, 
+    padding: 12
+  }
 });
